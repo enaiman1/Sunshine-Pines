839ac928a772fdc2655818278ab638d84d4ffff7
Ericcentrik <enaiman1@gmail.com>
2020-10-27 23:04:52 -0400

    created blog component, connected to contentful for blog management

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,19 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+
 
 module.exports = {
   siteMetadata: {
-    title: 'Sunshine Pines',
+    title: 'Full-stack Bootcamp!',
     author: 'Eric Naiman'
   }, 
   plugins: [
     'gatsby-plugin-react-helmet',
-  
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
+    },
     'gatsby-plugin-sass',
      {
        resolve: 'gatsby-source-filesystem',
