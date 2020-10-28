e657d8a991b8b67cd6b3b1256267709b133e151d
Ericcentrik <enaiman1@gmail.com>
2020-10-27 20:10:50 -0400

    added basic styles to components

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,32 @@ module.exports = {
     title: 'Sunshine Pines',
     author: 'Eric Naiman'
   }, 
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+  
+    'gatsby-plugin-sass',
+     {
+       resolve: 'gatsby-source-filesystem',
+       options: {
+         name: 'src',
+         path: `${__dirname}/src/`
+       }
+     },
+     'gatsby-plugin-sharp',
+     {
+       resolve: 'gatsby-transformer-remark',
+       options: {
+         plugins: [
+           'gatsby-remark-relative-images',
+           {
+             resolve: 'gatsby-remark-images',
+             options: {
+               maxWidth: 750,
+               linkImagesToOriginal: false
+             }
+           }
+         ]
+       }
+     }
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,19 @@
 import React from "react"
- 
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <Head title="Home" />
+       <h1>Hello.</h1>
+      <h2>This is the main page</h2>
+  
+    </Layout>
+  )
+
+}
+
+export default IndexPage
