aac0204bdbe518cd5286650d95830fad79e7d6d8
Ericcentrik <enaiman1@gmail.com>
2020-10-27 20:39:03 -0400

    added more style to header, footer, and adjusted layout style

--- src/components/header.js
+++ src/components/header.js
@@ -37,21 +37,22 @@ const Header = () => {
               <Link
                 className={headerStyles.navItem}
                 activeClassName={headerStyles.activeNavItem}
-                to="/blog"
-              >
-                Blog
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={headerStyles.navItem}
-                activeClassName={headerStyles.activeNavItem}
                 to="/about"
               >
                 About
               </Link>
             </li>
             <li>
+              <Link
+                className={headerStyles.navItem}
+                activeClassName={headerStyles.activeNavItem}
+                to="/blog"
+              >
+                Blog
+              </Link>
+            </li>
+            <li>
+              
               <Link
                 className={headerStyles.navItem}
                 activeClassName={headerStyles.activeNavItem}
